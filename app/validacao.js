68d05142538f7a339483c35837a1d1d595241748
dukeossuna <andregsistemas@outlook.com>
2023-11-07 16:18:23 -0300

    Projeto finalizado v2

--- app/validacao.js
+++ app/validacao.js
@@ -1,30 +1,41 @@
-function verificaSeChutePossuiUmValorValido() {
+function verificaSeChutePossuiUmValorValido(chute) {
     const numero = +chute
 
-    if(chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>';
-        return;
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+                `
+            document.body.style.backgroundColor = "black";
+        } else if (chute.toUpperCase() === "JOGAR NOVAMENTE") {
+            window.location.reload();
+        } else {
+            elementoChute.innerHTML += '<div>Valor inválido</div>';
+        }
     }
 
-    if(numeroMaiorOuMenorQuePermitido(numero)) {
+    if (numeroMaiorOuMenorQuePermitido(numero)) {
         elementoChute.innerHTML += `
         <div>Fale um número entre ${menorValor} e ${maiorValor}!
         `;
         return;
     }
 
-    if(numero === numeroSecreto) {
+    if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Parabéns, você acertou!</h2>
         <h3>O número secreto era o ${numeroSecreto}!</h3>
         <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
-       
-    }else if(numero > numeroSecreto) {
+
+    } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-down-long"></i> </div>
         `
-    }else {
+    } else {
         elementoChute.innerHTML += `
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i> </div>
         `
@@ -40,7 +51,7 @@ function numeroMaiorOuMenorQuePermitido(numero) {
 }
 
 document.body.addEventListener('click', e => {
-    if(e.target.id == 'jogar-novamente') {
+    if (e.target.id == 'jogar-novamente') {
         window.location.reload();
     }
 });
