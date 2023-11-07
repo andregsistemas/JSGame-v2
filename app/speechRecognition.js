e53c623ad054dc786752248e8e9a14654238e797
dukeossuna <andregsistemas@outlook.com>
2023-11-07 15:18:44 -0300

    Encerramento do Projeto

---
+++ app/speechRecognition.js
@@ -0,0 +1,26 @@
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+const numero = document.getElementById('box');
+
+recognition.lang = 'pt-Br';
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(event) {
+    chute = event.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+    verificaSeChutePossuiUmValorValido(chute);
+}
+
+function exibeChuteNaTela(chute){
+    elementoChute.innerHTML = `
+    <div>Você disse</div>
+    <span class="box">${chute}</span>
+    `;
+}
+
+recognition.addEventListener('end',() => recognition.start());
+
---
+++ app/validacao.js
@@ -0,0 +1,39 @@
+function verificaSeChutePossuiUmValorValido() {
+    const numero = +chute
+
+    if(chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return;
+    }
+
+    if(numeroMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML += `
+        <div>Fale um número entre ${menorValor} e ${maiorValor}!
+        `;
+        return;
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Parabéns, você acertou!</h2>
+        <h3>O número secreto era o ${numeroSecreto}!</h3>
+        `
+       
+    }else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i> </div>
+        `
+    }else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i> </div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
